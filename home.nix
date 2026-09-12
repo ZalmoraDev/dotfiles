6d0f39f5d19464768e23d253f5a384b9a7120d75
Stef Visser <vertex4004@protonmail.com>
2026-09-12 19:18:30 +0200

    added dolphin dotfiles

--- home.nix
+++ home.nix
@@ -22,31 +22,45 @@ in
 #     '';
 #   };
 
-    home.file.".bashrc".source = dotfiles + "/bash/.bashrc";                                  # 2026-09-10 | bashrc
-    home.file.".bash_profile".source = dotfiles + "/bash/.bash_profile";                      # 2026-09-10 | bash_profile
 
-    #home.file.".clangformat".source = dotfiles + "/clang/.clangformat";                      # 2026-09-10 | clangformat
+  # Commented lines contain sentsitive data, figure out how to .gitignore a setup for this
+  home.file.".bashrc".source = dotfiles + "/bash/.bashrc";                                              # 2026-09-10 | bash
+  home.file.".bash_profile".source = dotfiles + "/bash/.bash_profile";                                  # 2026-09-10 | bash
+  home.file.".bash_logout".source = dotfiles + "/bash/.bash_logout";                                    # 2026-09-12 | bash
 
-    home.file.".config/fastfetch".source = dotfiles + "/fastfetch/.config/fastfetch";         # 2026-09-10 | fastfetch
+  #home.file.".config/blender".source = dotfiles + "/blender/.config/blender";                           # 2026-09-12 | blender
 
-    home.file.".config/ghostty".source = dotfiles + "/ghostty/.config/ghostty";               # 2026-09-10 | ghostty
+  home.file.".clangformat".source = dotfiles + "/clang/.clangformat";                                   # 2026-09-10 | clangformat
 
-    #home.file.".gitconfig".source = dotfiles + "/git/.gitconfig";                            # 2026-09-10 | git
+  home.file.".config/dolphinrc".source = dotfiles + "/dolphin/.config/dolphinrc";                       # 2026-09-12 | dolphin
 
-    home.file.".config/hypr".source = dotfiles + "/hypr/.config/hypr";                        # 2026-09-10 | hyprland
+  home.file.".config/fastfetch".source = dotfiles + "/fastfetch/.config/fastfetch";                     # 2026-09-10 | fastfetch
 
-    home.file.".config/mimeapps.list".source = dotfiles + "/mimeapps/.config/mimeapps.list";  # 2026-09-10 | mimemapps
+  home.file.".config/ghostty".source = dotfiles + "/ghostty/.config/ghostty";                           # 2026-09-10 | ghostty
 
-    home.file.".config/rofi".source = dotfiles + "/rofi/.config/rofi";                        # 2026-09-10 | rofi config
-    home.file.".local/share/rofi".source = dotfiles + "/rofi/.local/share/rofi";              # 2026-09-10 | rofi thmes
+  home.file.".gitconfig".source = dotfiles + "/git/.gitconfig";                                         # 2026-09-10 | git
 
-    home.file.".config/starship.toml".source = dotfiles + "/starship/.config/starship.toml";  # 2026-09-10 | starship
+  home.file.".config/hypr".source = dotfiles + "/hypr/.config/hypr";                                    # 2026-09-10 | hyprland
 
-    home.file.".tmux.conf".source = dotfiles + "/tmux/.tmux.conf";            # 2026-09-10 | tmux
+  home.file.".config/mimeapps.list".source = dotfiles + "/mimeapps/.config/mimeapps.list";              # 2026-09-10 | mimemapps
 
-    home.file.".config/waybar".source = dotfiles + "/waybar/.config/waybar";                  # 2026-09-10 | waybar
+  home.file.".nanorc".source = dotfiles + "/nano/.nanorc";                                              # 2026-09-12 | nano
+  home.file.".local/share/nano".source = dotfiles + "/nano/.local/share/nano";                          # 2026-09-12 | nano
 
+  #home.file.".config/PureRef/PureRef.ini".source = dotfiles + "/pureref/.config/PureRef/PureRef.ini";   # 2026-09-12 | pureref
+
+  home.file.".config/rofi".source = dotfiles + "/rofi/.config/rofi";                                    # 2026-09-10 | rofi config
+  home.file.".local/share/rofi".source = dotfiles + "/rofi/.local/share/rofi";                          # 2026-09-10 | rofi themes
+
+  home.file.".config/starship.toml".source = dotfiles + "/starship/.config/starship.toml";              # 2026-09-10 | starship
+
+  home.file.".tmux.conf".source = dotfiles + "/tmux/.tmux.conf";                                        # 2026-09-10 | tmux
+
+  home.file.".config/waybar".source = dotfiles + "/waybar/.config/waybar";                              # 2026-09-10 | waybar
+
+  #home.file.".ssh".source = dotfiles + "/ssh/.ssh";                                                     # 2026-09-12 | ssh
+
+  
   home.packages = with pkgs; [
-    
   ];
 }
